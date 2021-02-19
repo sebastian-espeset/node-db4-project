5cf918c8958a222e905bdad2d3bc61badd9a5c0a
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-02-18 18:19:01 -0600

    mvp met, but want to rewrite the data base schema

--- api/server.js
+++ api/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const server = express();
+const RecipesRouter= require('../api/recipes/recipes-router')
 
-server.use(express.json())
+server.use(express.json());
+server.use('/api/recipes',RecipesRouter);
+
+server.get('/',(req,res)=>{
+    res.status(200).json(`hello bubblegum`)
+})
 
 module.exports=server;
